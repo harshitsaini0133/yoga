e2abc744f2b06f4cde066da64140ec8e68d96661
Nishant Gupta <nishant.bit@outlook.com>
2026-03-01 00:53:33 +0530

    updated 0.1.8

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -6,11 +6,13 @@
 
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-const prisma = new PrismaClient({
-  adapter: new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-  }),
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
 });
+
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
